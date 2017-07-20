42b52d49c366bdefb970fccf5e0cc24bc4d84724
Ice Zero <ziv3@outlook.com>
2017-07-20 21:37:40 +0800

    update .gitignore

--- elpa/jquery-doc-readme.txt
+++
@@ -1,1 +0,0 @@
-provides completion source for autocomplete, documentation lookup
--- elpa/markdown-preview-eww-20160111.702/markdown-preview-eww-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; markdown-preview-eww-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("markdown-preview-eww.el") (22896 31171
-;;;;;;  423364 849000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; markdown-preview-eww-autoloads.el ends here
