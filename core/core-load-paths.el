30057c79cc828a9c65927ee46370a4c2da38b3b4
Ice Zero <ziv3@outlook.com>
2017-11-12 01:22:37 +0800

    ERROR SAVE

--- core/core-load-paths.el
+++ core/core-load-paths.el
@@ -4,6 +4,8 @@
   "load emacs config file"
   (load-file (concat user-emacs-directory args/path)))
 
+(add-to-list (concat user-emacs-directory "core/"))
+
 (load-router "core/core-start.el")
 (load-router "components/components-start.el")
 (load-router "plugins/plugins-start.el")
